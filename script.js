cd4e651a0c845bb8c553d7d3fca1016698e8c0d8
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-10 12:34:23 +0530

    base JS

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ const operations = {
 }
 let uptext = ""       // Text for upper box
 let lowtext = ""           // Text for lower box
+let bool = false;
 
-let num1 = null;
+let num1 = 0;
 let num2 = 0;
 let ans = "";
 let operator = "";
@@ -79,13 +80,68 @@ for(let i = 0; i < buttons.length; i++){
         }
         
         else if(buttons[i].dataset.set in operations && lowtext != ""){
-            
+            operator = buttons[i].dataset.key
+            if(num1 == 0){            
+                console.log("First operation condtion")              // For the first number !
+                num2 = Number(lowtext)
+                ans = num2
+                num1 = ans                          // Previous Answer
+                uptext += operator
+                upperDisplay.textContent = uptext
+                lowerDisplay.textContent = num1
+                lowtext = ""
+                
+            }
+            else if(num1 != 0 && !bool){
+                
+                operator = buttons[i].dataset.key
+                console.log("Second operation condtion")
+                num2 = Number(lowtext)
+                ans =  operate(num1, num2, operator)
+                num1 = ans
+                lowtext = ""
+                lowerDisplay.textContent = num1
+                upperDisplay.textContent = uptext
+                uptext += operator
+                
+            }
+            else if(bool){
+                lowtext = ""
+                num2 = Number(lowtext)
+                ans =  operate(num1, num2, operator)
+                num1 = ans
+                lowerDisplay.textContent = num1
+                uptext += operator
+                upperDisplay.textContent = uptext
+                
+
+            }
             
         }
         
         else if(buttons[i].dataset.set == "equal"){
+            if(num1 == 0){
+                console.log("First = condtion")   
+                num2 = Number(lowtext)
+                upperDisplay.textContent = num2
+                lowerDisplay.textContent = num2
+                bool = true
+            }
+            else{
+                console.log("Second = condtion")   
+                num2 = Number(lowtext)
+                ans =  operate(num1, num2, operator)
+                num1 = ans
+                lowerDisplay.textContent = num1
+                upperDisplay.textContent = uptext
+                bool = true
+            }
+            
         }
     }
 
     )
 }
+
+
+
